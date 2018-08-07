20a456025fb0955138026250058a8be3a61cc697
Stephan Fischer <stephischer@gmail.com>
2018-08-07 13:55:52 -0400

    Use quantiles of log gene expression instead of median expression

--- variableGenes.R
+++ variableGenes.R
@@ -6,9 +6,9 @@
 #' @param dat SummarizedExperiment object containing gene-by-sample
 #' expression matrix.
 #' @param i default value 1; non-zero index value of assay containing the matrix data
-#' @param exp_labels character vector that denotes the source (Study ID) of 
+#' @param exp_labels character vector that denotes the source (Study ID) of
 #' each sample.
-#' 
+#'
 #' @return The output is a vector of gene names that are highly variable in
 #' every experiment (intersect)
 #'
@@ -21,57 +21,44 @@
 #'
 
 variableGenes <- function(dat, i = 1, exp_labels) {
-    
-    dat <- SummarizedExperiment::assay(dat, i = i)
-    var_genes1 <- vector("list")
-    j <- 1
-    
-    #check length of exp_labels equal # of samples
-    if(length(exp_labels) != length(colnames(dat))){
-        stop('experiment_labels length does not match number of samples')
-    }
-    
-    #check obj contains more than 1 unique study_id
-    if(length(unique(exp_labels)) < 2){
-        stop('Found only 1 unique exp_labels. Please use data from more than 1 study!')
-    }
-    
-    
-    experiments <- unique(exp_labels)
-    for(exp in experiments){
-        data_subset   <- dat[ , exp_labels == exp]
-        genes_list    <- vector("list")
-        median_data   <- apply(data_subset, MARGIN = 1, FUN = stats::median)
-        variance_data <- apply(data_subset, MARGIN = 1, FUN = stats::var)
-        quant_med     <- unique(stats::quantile(median_data,
-                                                probs = seq(from = 0,
-                                                            to = 1,
-                                                            length = 11),
-                                                type = 5))
-        genes_list    <- vector("list", length = length(quant_med))
+  dat <- SummarizedExperiment::assay(dat, i = i)
+  check_variable_gene_input(dat, exp_labels)
+  result <- vector("list")
+  for (exp in unique(exp_labels)) {
+    gene_list <- variable_genes_from_experiment(dat[ , exp_labels == exp])
+    # we remove the last bin, corresponding to highly expressed genes
+    result[[length(result) + 1]] <- unlist(gene_list[-length(gene_list)])
+  }
+  return(result)
+}
 
-        for(i in seq_along(quant_med)){
-            if(i == 1){
-                filt1     <- median_data <= quant_med[i]
-                var_temp  <- variance_data[filt1]
-                quant_var <- stats::quantile(var_temp, na.rm = TRUE)
-                filt2     <- var_temp > quant_var[4]
-                genes_list[[i]] <- names(var_temp)[filt2]
-            } else {
-                filt1     <- median_data <= quant_med[i] & median_data > quant_med[i-1]
-                var_temp  <- variance_data[filt1]
-                quant_var <- stats::quantile(var_temp, na.rm = TRUE)
-                filt2     <- var_temp > quant_var[4]
-                genes_list[[i]] <- names(var_temp)[filt2]
-            }
-        }
+check_variable_gene_input <- function(dat, exp_labels) {
+  if(length(exp_labels) != length(colnames(dat))){
+      stop('experiment_labels length does not match number of samples')
+  }
+  if(length(unique(exp_labels)) < 2){
+      stop('Found only 1 unique exp_labels. Please use data from more than 1 study!')
+  }
+}
 
-        temp <- length(genes_list)
-        var_genes1[[j]] <- unlist(genes_list[1:temp-1])
-        j <- j+1
-    }
-    var_genes <- Reduce(intersect, var_genes1)
-    return(var_genes)
+variable_genes_from_experiment <- function(data_subset) {
+  quantile_data <- get_deciles(data_subset[Matrix::rowSums(data_subset) > 0, ])
+  variance_data <- apply(data_subset, MARGIN = 1, FUN = stats::var)
+  result <- vector("list", length = length(levels(quantile_data)))
+  for(i in seq_along(levels(quantile_data))) {
+    var_temp <- variance_data[quantile_data == levels(quantile_data)[i]]
+    quant_var <- stats::quantile(var_temp, na.rm = TRUE)
+    result[[i]] <- names(var_temp)[var_temp > quant_var[4]]
+  }
+  return(result)
+}
+
+get_deciles <- function(data_subset) {
+  geom_mean <- apply(data_subset, MARGIN = 1, FUN = function(x) sum(log1p(x)))
+  quants <- stats::quantile(geom_mean,
+                            probs = seq(from = 0, to = 1, length = 11),
+                            type = 5)
+  return(cut(geom_mean, quants, include.lowest = TRUE))
 }
 
 extract_variable_genes <- function(dat, var_genes) {
@@ -86,4 +73,3 @@ extract_variable_genes <- function(dat, var_genes) {
   }
   return(dat[!is.na(matching_vargenes),])
 }
-
